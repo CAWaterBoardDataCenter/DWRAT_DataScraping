4894a321ae2c9db89ecfc2e2547e591e40b3e35a
Paul <Manashi.Paul@Waterboards.ca.gov>
2023-03-21 16:12:49 -0700

    updated on 03/21

--- Scripts/Final_datfile.R
+++ Scripts/Final_datfile.R
@@ -1,16 +1,34 @@
 library(here)
+library(dplyr)
 default_folder = here("DWRAT_DataScraping3")
 #load dat_shell dataframe, RAW_processed dataset, and PRISM dataset
 Stations = read.csv(here("InputData/Raws_Stations.csv"))
+
 df1<-read.csv(here("InputData/Dat_shell.csv"))
 df2<-read.csv(here("ProcessedData/RAWS_Processed.csv"))
 df3<-read.csv(here("ProcessedData/PRISM_Processed.csv"))
 
 #find the unique station names
-station_names <- unique(df1$station_name)
+station_names <- unique(colnames(df1))
 
 # Merge the two data frames based on the station name column
-merged_data <- merge(df1, df2, by = "station_name")
+merged_data <- merge(df1, df2, by = "Date")
+merged_data <- left_join(df1, df2, by = "Date") 
+
+# Define a new data frame to store the results
+result_df <- data.frame(Date = unique(merged_data$Date), stringsAsFactors = FALSE)
+ i=1 
+ j=1
+# Loop through the rows and columns of the merged data frame and add the values
+for (i in 1:nrow(merged_data)) {
+  for (j in 2:ncol(merged_data)) {  # skip the station name column
+    if (!is.na(merged_data[i, j])) {
+      result_df[merged_data[i, 1] == result_df$Date, colnames(merged_data)[j]]  
+        result_df[merged_data[i, 1] == result_df$Date, colnames(merged_data)[j]] + merged_data[i, j]
+    }
+  }
+}
+
 
 # Loop through the rows and columns of the merged data frame and add the values
 for (i in 1:nrow(merged_data)) {
