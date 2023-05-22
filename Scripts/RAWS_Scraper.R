3fd6ced0bc4f9263830aed2e137f37f17056ef7a
Payman Alemi <payman.alemi@waterboards.ca.gov>
2023-05-22 15:58:47 -0700

    Update RAWS_Scraper.R to automatically detect latest Chrome version

--- Scripts/RAWS_Scraper.R
+++ Scripts/RAWS_Scraper.R
@@ -1,6 +1,6 @@
 #SCRIPT LAST UPDATED:
     #BY: Payman Alemi
-    #ON: 5/8/2023
+    #ON: 5/22/2023
 
 #install packages----
   #you should only have to do this once ever on your computer; then comment
@@ -23,24 +23,70 @@ library(dplyr)
 library(readr)
 
 #Set up RSelenium----
-##Set Default download folder ----
+##Set Default download folder----
 eCaps <- list(
-  chromeOptions =
-    list(prefs = list(
+  chromeOptions = list(
+    prefs = list(
       "profile.default_content_settings.popups" = 0L,
       "download.prompt_for_download" = FALSE,
       "download.default_directory" = gsub(pattern = '/', replacement = '\\\\', x = here("WebData")) # download.dir
     )
-    )
+  )
 )
-default_folder = here("WebData")
-## Open browser ----
+default_folder <- eCaps$chromeOptions$prefs$download.default_directory
+
+## Set version of Chrome----
+### Get current version of chrome browser----
+chrome_browser_version <- system2(
+  command = "wmic",
+  args = 'datafile where name="C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe" get Version /value',
+  stdout = TRUE,
+  stderr = TRUE
+) %>%
+  str_extract(pattern = "(?<=Version=)(\\d+\\.){3}")
+
+if (sum(!is.na(chrome_browser_version)) == 0) {
+  chrome_browser_version <- system2(
+    command = "wmic",
+    args = 'datafile where name="C:\\\\Program Files\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe" get Version /value',
+    stdout = TRUE,
+    stderr = TRUE
+  ) %>%
+    str_extract(pattern = "(?<=Version=)(\\d+\\.){3}")
+}
+
+###List the versions of chromedriver on this PC----
+chrome_driver_versions <- list_versions("chromedriver")
+
+###Match drivers to version----
+chrome_driver_current <- chrome_browser_version %>%
+  magrittr::extract(!is.na(.)) %>%
+  str_replace_all(pattern = "\\.", replacement = "\\\\.") %>%
+  paste0("^", .) %>%
+  str_subset(string = last(chrome_driver_versions)) %>%
+  as.numeric_version() %>%
+  max() %>%
+  as.character()
+
+### Remove the LICENSE.chromedriver file (if it exists)----
+chrome_driver_dir <- paste0(app_dir("chromedriver", FALSE),
+                            '/win32/',
+                            chrome_driver_current)
+if ('LICENSE.chromedriver' %in% list.files(chrome_driver_dir)) {
+  file.remove(
+    paste0(chrome_driver_dir, '/', 'LICENSE.chromedriver')
+  )
+}
+
+##Open a chrome browser session with RSelenium ----
 rs_driver_object <-rsDriver(
   browser = 'chrome',
-  chromever ='113.0.5672.63',
+  chromever = chrome_driver_current, #set to the version on your PC that most closely matches the chrome browser version
   port = free_port(),
+  extraCapabilities = eCaps
 )
 
+Sys.sleep(1)
 remDr <- rs_driver_object$client
 
 #Input Data----
@@ -49,7 +95,7 @@ Stations = read.csv(here("InputData/Raws_Stations.csv"))
 
 #Define Timeframe for which you're downloading observed data
 StartDate = data.frame("April", "01", "2023", as.Date("2023-04-01"))
-EndDate = data.frame("May", "07", "2023", as.Date("2023-05-07"))
+EndDate = data.frame("May", "21", "2023", as.Date("2023-05-21"))
 
 colnames(StartDate) = c("month", "day", "year", "date")
 colnames(EndDate) = c("month", "day", "year", "date")
