224724a6b06692739c895442da4a341f76ac4299
Payman Alemi <Payman.Alemi@waterboards.ca.gov>
2023-09-06 15:21:28 -0700

    Update Multiple owner script

--- Demand/Scripts/Multiple_Owner_Analysis.R
+++ Demand/Scripts/Multiple_Owner_Analysis.R
@@ -8,20 +8,11 @@ library(data.table) #for fread function
 #script; but we just want the unique APPLICATION_NUMBERS; these are just water rights
 #in the Russian River.
 appYears <- read_csv("IntermediateData/Statistics_FINAL.csv", show_col_types = FALSE) %>%
-  select(APPLICATION_NUMBER, YEAR, MONTH, AMOUNT, DIVERSION_TYPE) %>% unique()
+  select(APPLICATION_NUMBER, YEAR, MONTH, AMOUNT, DIVERSION_TYPE) %>% unique() %>%
 
+  #Add a YEAR_ID column that concatenates the year and application number
+  mutate(YEAR_ID = paste(YEAR, APPLICATION_NUMBER, sep = "_"))
 
-#Import ewrims_flat_file_party.csv--this contains party information for all 
-#water rights in eWRIMS, not just the Russian River.
-
-  # Filter it down to only "Primary Owner" records and "APPLICATION_ID" values that
-  # match "APPLICATION_NUMBER" in 'appYears'
-  # Along with that, the "EFFECTIVE_TO_DATE" should be NA (currently active), 
-  # or the ownership ended during the dataset's timeframe ("EFFECTIVE_TO_DATE" is between 2017-present)
-partyDF <- read_csv("RawData/ewrims_flat_file_party.csv", col_types = cols(.default = col_character())) %>%
-  filter(RELATIONSHIP_TYPE == "Primary Owner" & APPLICATION_ID %in% appYears$APPLICATION_NUMBER) %>%
-  mutate(EFFECTIVE_TO_YEAR = as.numeric(str_extract(EFFECTIVE_TO_DATE, "[0-9]{4}$"))) %>%
-  filter(is.na(EFFECTIVE_TO_DATE) | EFFECTIVE_TO_YEAR >= min(appYears$YEAR))
 
 #Import the first 10 rows of the water use report extended flat file
 extended <- read.csv("RawData/water_use_report_extended.csv", nrows = 10)
@@ -39,7 +30,8 @@ write.csv(extended_column_names, file = "IntermediateData/extended_names.csv")
   #Column 65 = PARTY_ID, integer
 
 file_path <- "RawData/water_use_report_extended.csv"
-selected_columns <- c("APPL_ID", "YEAR", "APPLICATION_PRIMARY_OWNER", "PARTY_ID")
+selected_columns <- c("APPL_ID", "YEAR", "AMOUNT", "DIVERSION_TYPE",
+                      "APPLICATION_PRIMARY_OWNER", "PARTY_ID")
 
 #Import only the selected_columns of the water_use_report_extended.csv
 RMS_parties <- fread(file = file_path, select = selected_columns)
@@ -54,6 +46,32 @@ RMS_parties <- unique(RMS_parties) %>% data.frame()
 #Filter RMS_parties to just the Russian River
 RMS_parties <- RMS_parties %>%
   filter(APPL_ID %in% appYears$APPLICATION_NUMBER)
+
+#Export RMS_parties to Excel for manual analysis----
+#write.xlsx(x = RMS_parties, file = "IntermediateData/RMS_parties.xlsx")
+
+#Look for duplicate party IDs ----
+#Add a Year_Right column to RMS_parties
+RMS_parties <- RMS_parties %>%
+  mutate(YEAR_ID = paste(YEAR, APPL_ID, sep = "_"))
+
+#Group RMS_parties by YEAR_ID
+  #the purpose is to catch any instances where more than 1 party ID was
+  #affiliated with an annual water right report--if so, that's a problem
+
+#Remove diversion data columns--NDD = No Diversion Data
+RMS_parties_NDD = select(RMS_parties, -c("AMOUNT", "DIVERSION_TYPE")) %>%unique()
+
+RMS_parties_aggregate = RMS_parties_NDD %>% group_by(YEAR_ID) %>%
+  summarise(PartyCount = n()) %>%
+  arrange(desc(PartyCount)) #None of the PartyCounts exceed 1; so we have no
+  #duplicate party IDs!
+
+#Look for cases where the year, party ID, diversion type, and amount are duplicated----
+  #We will create a new field, PK, short for primary key, that concatenates these fields:
+    # YEAR, PARTY_ID, DIVERSION_TYPE, AMOUNT; 
+  #we will separate the fields with an underscore(_) for legibility
+    #
 
 
 
