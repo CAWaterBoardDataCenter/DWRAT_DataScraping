e9cd820642328b80f505f12e2cbcffbd422a89f0
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2024-04-04 14:46:50 -0700

    Changed formatting of some filenames and commented out CNRFC scripts

--- Supply/Scripts/Master_Script_PRMS.R
+++ Supply/Scripts/Master_Script_PRMS.R
@@ -52,8 +52,8 @@ source(here("Scripts/PRISM_HTTP_Scraper.R")) #downloads PRISM climate data for b
 source(here("Scripts/PRISM_Processor.R"))
 print(Prism_Processed)
 source(here("Scripts/NOAA_API_Scraper.R"))
-source(here("Scripts/CNRFC_API_Scraper.R")) #downloads CNRFC data for both PRMS and SRP stations simultaneously
-source(here("Scripts/CNRFC_PRMS_Processor.R")) #Formats CRNFC station data that are used by the PRMS model so 
+#source(here("Scripts/CNRFC_API_Scraper.R")) #downloads CNRFC data for both PRMS and SRP stations simultaneously
+#source(here("Scripts/CNRFC_PRMS_Processor.R")) #Formats CRNFC station data that are used by the PRMS model so 
   # they can be appended to the raw observed datasets from RAWS, CIMIS, and NOAA
 print(CNRFC_Processed)
 # change input file name for Downsizer data; you need to run Downsizer and  
