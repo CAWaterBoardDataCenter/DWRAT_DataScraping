786608dad370b4e04b2e20f24bf8a59978c17096
Payman Alemi <Payman.Alemi@waterboards.ca.gov>
2024-04-25 14:55:53 -0700

    Updated SPI_Dat_SRP_Generator.R script with basic instructions, also imported SRP dat file with observed data through March, forecast data for late March, SPI data from 4/1 to 9/30/2024

--- Supply/Scripts/SPI_Dat_SRP_Generator.R
+++ Supply/Scripts/SPI_Dat_SRP_Generator.R
@@ -7,15 +7,51 @@ library(data.table) #for fread function
 library(readxl) #for read_xlsx function
 
 
-# Rely on the shared functions from the Demand scripts
+# Rely on the shared functions from the Demand and Supply scripts
 source("../Supply/Scripts/Shared_Functions_Supply.R")
+source("../Demand/Scripts/Shared_Functions_Demand.R")
 
 # Import Latest Dat SRP File
+Dat_SRP_Path = makeSharePointPath(filePathFragment = "DWRAT\\SDU_Runs\\Hydrology\\DAT SRP Blueprints\\Dat_SRP_Body.dat")
 
-  # Divide the SRP File into a pre-WY 2021 portion
-  # Rely on observed data to fill in 10/1/2020 - 9/30/2023
-  # Save the observed portion from 10/1/2020 - 9/30/2023 and pre-WY 2021 into a
-  # single SRP Dat pre-WY 2023 file, 1/1/1947 - 9/30/2023
-  # Write code that downloads the observed data from 10/1/2023 - 3/31/2024
-  # Generate an SPI portion corresponding to 4/1/2024 - 9/30/2024
 
+# Read the data as a single column
+Dat_SRP_Full <- read_lines(Dat_SRP_Path)
+
+# Define a regular expression pattern to split the data based on one or more spaces
+pattern <- "\\s+"
+
+# Split the data into separate columns based on the pattern
+split_data <- str_split(Dat_SRP_Full, pattern)
+
+# Convert the list of vectors into a dataframe
+Dat_SRP_Full <- as.data.frame(do.call(rbind, split_data))
+Dat_SRP_Full$V13 = NULL
+
+#Import Dat SRP Field Names
+Dat_SRP_Fields_Path = makeSharePointPath("DWRAT\\SDU_Runs\\Hydrology\\DAT SRP Blueprints\\Dat_SRP_FieldNames.csv")
+Dat_SRP_Fields = read.csv(file = Dat_SRP_Fields_Path, header = F) %>% unlist()
+
+colnames(Dat_SRP_Full) = Dat_SRP_Fields
+
+#DAT_SRP_Full needs to subdivided into 2 separate standalone CSVs
+  #A file consisting of data from 1/1/1990 - 9/30/2023; that's our pre-2023WY_SRP_DAT file
+    #Save to DAT SRP Blueprints folder on SharePoint
+  #A file consisting of data from 4/1/2024 - 9/30/2024; that's our SPI WY 2023-2024 DAT file or Dat SRP Forecast file for short
+    #Save to DAT SRP Blueprints folder on SharePoint
+
+#Constructing a new SRP Dat File
+  #Run the first portion of Master_Script_PRMS.R where you define StartDate, EndDate, and you download the SRP raw data
+  #Import Pre-2023 WY SRP DAT file
+  #Import SPI WY 2023-2024 SRP DAT File
+  #Import SRP_Processed.csv
+
+  #Have some logic that overrides overlapping portion of SPI file with observed data if observed data exists. 
+  #Never use SPI data if you have observed data available.
+  #combine the 3 datasets (Pre-2023 WY SRP DAT File, SPI data, and observed data) to generate final SRP DAT file
+  # for a specific month
+  #Save the SRP_Dat file for a specific month to the ProcessedData folder with a timestamp. The timestamp is the EndDate;
+  #EndDate is the last day of the observed data range. 
+
+# Import Processed SRP data
+SRP_Processed = read.csv(file = "ProcessedData/SRP_Processed.csv")
