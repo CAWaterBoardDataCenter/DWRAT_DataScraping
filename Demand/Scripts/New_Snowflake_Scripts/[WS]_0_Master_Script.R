bf98d3f9b0fc75659d8fd0d8a80baa9442644f2e
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2025-03-24 16:25:14 -0700

    Starting on [WS] scripts

--- Demand/Scripts/New_Snowflake_Scripts/[WS]_0_Master_Script.R
+++ Demand/Scripts/New_Snowflake_Scripts/[WS]_0_Master_Script.R
@@ -1,5 +1,23 @@
+# Given the eWRIMS flag table (generated using "[CA]_0_Master_Script.R"),
+# Develop a QA/QC'd demand dataset for a given watershed
+
+
+remove(list = ls())
 
 
 
+require(cli)
+require(tidyverse)
+require(readxl)
 
+
+
+#### IMPORTANT! ####
+
+# CHOOSE YOUR WATERSHED #
+source("Scripts/New_Snowflake_Scripts/[HELPER]_3_Watershed_Selection.R")
+
+
+
+# CHOOSE THE BOUNDS OF YOUR DEMAND DATASET #
 
