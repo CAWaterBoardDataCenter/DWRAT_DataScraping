fc2d2efefbb3a1d8596949a3a8917fb9839caa0d
Payman Alemi <Payman.Alemi@waterboards.ca.gov>
2025-05-22 15:24:12 -0700

    Resolved merge conflicts in MasterDemandTable and Watershed_Selection

--- Demand/Scripts/Watershed_Selection.R
+++ Demand/Scripts/Watershed_Selection.R
@@ -15,8 +15,12 @@ source("Scripts/Shared_Functions_Demand.R")
 
 
 # IMPORTANT!! CHOOSE A WATERSHED
+<<<<<<< Updated upstream
 index <- 6 # Change the index to your desired watershed's corresponding "INDEX" value
 
+=======
+ws <- ws[9, ] # Change the row index to your desired watershed
+>>>>>>> Stashed changes
 
 
 
