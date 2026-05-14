7ef996e5c3b16734633452b466920a44fbf5616f
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2026-05-14 15:45:00 -0700

    PRISM grid cell precipitation averages are archived regardless of whether they are used in the procedure (this data will be useful for the QA/QC charts)

--- Supply/Scripts/RRW_013_Finalize_SRP_Input.R
+++ Supply/Scripts/RRW_013_Finalize_SRP_Input.R
@@ -614,11 +614,8 @@ similarWYPrediction <- function (mergedDAT, pastPrecip, endDate,
                      similarWY = similarWY, linModel = linModel)
   
   
-  # Copy 'currentPrecip' to the hydrology folder as well
-  copyFile(prismPath,
-           paste0(dirPath, "/SRP/Input/",
-                  prismPath |> str_remove("^.+[/\\\\]")) |>
-             normalizePath(mustWork = FALSE))
+  # 'currentPrecip' shoul be archived too, but that was already accomplished
+  # in a prior script
   
   
   # Return 'finalDAT'
