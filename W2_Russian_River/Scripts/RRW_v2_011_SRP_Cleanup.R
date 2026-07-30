5460b2ce37579e84f618c9f9ea594c49d5ecbc61
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2026-07-30 16:29:18 -0700

    More streamlining and updates through the end of the SRPHM run

--- W2_Russian_River/Scripts/RRW_v2_011_SRP_Cleanup.R
+++ W2_Russian_River/Scripts/RRW_v2_011_SRP_Cleanup.R
@@ -63,7 +63,11 @@ mainProcedure <- function () {
   cat("[3/3]\tDeleting the model files...\n")
   
   
-  deleteFiles(srpPath)
+  # Import the model deletion function from the PRMS script
+  functionStealer("W2_Russian_River/Scripts/RRW_011_PRMS_Cleanup.R", "deleteFiles")
+  
+  
+  deleteFiles(srpPath, "SRP")
   
   
   cat("\tDone!\n\n")
@@ -110,10 +114,11 @@ copyOutputs <- function (srpPath, dirPath, startDate, endDate) {
   
   
   # Add rows to 'sourcePaths' and 'writePaths' for other important files, 
-  # like "SRPHM_spinup.control", "SRPHM_spinup.nam", and "restartdata_2020.out"
-  otherFiles <- c("/model1/SRPHM_post_spinup_WY2021/SRPHM_spinup.control",
-                  "/model1/SRPHM_post_spinup_WY2021/SRPHM_spinup.nam",
-                  "/external_files/restartdata_2020.out")
+  # like "SRPHM_spinup.control", "SRPHM_spinup.nam", and "restartdata_WY1975_to_WY2021.out"
+  otherFiles <- c("/SRPHM_spinup.control",
+                  "/SRPHM_spinup.nam",
+                  "/external_files/prms_ic_WY1975_toWY2021.out",
+                  "/external_files/restartdata_WY1975_to_WY2021.out")
   
   
   sourcePaths <- c(sourcePaths,
@@ -136,33 +141,6 @@ copyOutputs <- function (srpPath, dirPath, startDate, endDate) {
   
 }
 
-
-
-deleteFiles <- function (srpPath) {
-  
-  # Delete the "SRPHM" directory in the "Output" folder
-  
-  # Start by deleting that folder
-  dir_delete(srpPath)
-  
-  
-  # Confirm that it was deleted
-  if (dir.exists(srpPath)) {
-    
-    stop(paste0("Failed to Delete SRP Directory\n\n",
-                "The script attempted to delete the SRP model files that ",
-                "were located in the \"Output\" folder. However, it ",
-                "was unsuccessful for an unknown reason. Please investigate.\n\n",
-                "(This error occurred for \"", srpPath, "\")") |>
-           errWrap())
-    
-  }
-  
-  
-  # Return nothing
-  return(invisible(NULL))
-  
-}
 
 
 #### Script Execution ####
