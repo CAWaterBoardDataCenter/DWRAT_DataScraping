16dfcd7a334778ca87c9c9cdc43718252c72c332
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2026-08-17 10:35:10 -0700

    New function that's meant to clarify to code readers what a `str_remove` command that I often use does; also some formatting updates to documentation

--- Package/R/Streamliner.R
+++ Package/R/Streamliner.R
@@ -6,6 +6,56 @@
 # (as opposed to edits wherever the assumption is applied)
 
 
+#' @title Extract a File Name from a File Path 
+#' 
+#' @description
+#' This function is a shortcut for applying [stringr::str_remove()] to remove 
+#' all folders from a file path. For example, given a path like 
+#' "C:/Users/.../example.txt" or "C:\\Users\\...\\example.txt", this function 
+#' can return "example.txt". 
+#' 
+#' @details
+#' 
+#' This function relies on the default greedy behavior of pattern matching with 
+#' [stringr::str_remove()]. The regular expression in `extract_filename` matches 
+#' with everything in a string from its beginning to a backslash or forward slash. 
+#' With a [greedy approach](https://stackoverflow.com/questions/2301285/what-do-lazy-and-greedy-mean-in-the-context-of-regular-expressions),
+#' this will match everything in a file path until the final slash. The end result
+#' of [stringr::str_remove()] will be the remnants of the string after that slash; 
+#' this should be just the filename. 
+#' 
+#' @usage extract_file(path) 
+#' 
+#' @param path A [character] vector containing one or more file paths.
+#' 
+#' @returns A [character] [vector] with the same size as `path`.
+#' 
+#' @export
+#' 
+#' @examples
+#' # Given a vector of paths, extract the filename portion
+#' paths <- c("test.txt", "example/test.txt", "example_2\\test.txt")
+#' 
+#' # The function works with either a single string or multiple elements
+#' extract_filename(paths[1])
+#' 
+#' extract_filename(paths)
+extract_filename <- function (path) {
+  
+  # Extract a filename from a file path
+  
+  # (e.g., getting "example.txt" from "C:/Users/.../example.txt")
+  
+  return(path |>
+           str_remove("^.+[/\\\\]"))
+  
+  # This regular expression removes everything from the beginning of the string
+  # to a forward slash or backslash 
+  # (since the search is greedy by default, it will stop at the final slash)
+  
+}
+
+
 #' @title Find a Match in a Vector
 #' 
 #' @description
@@ -36,38 +86,38 @@
 #'              returnIndex = TRUE, filePath = NULL, 
 #'              ignore.case = FALSE, fixed = FALSE) 
 #' 
-#' @param x A [character()] vector that will be searched for matches.
+#' @param x A [character] vector that will be searched for matches.
 #' 
-#' @param pattern A [character()] string that contains a regular expression. This
+#' @param pattern A [character] string that contains a regular expression. This
 #' will be applied to `x` to search for matches.
 #' 
-#' @param minMatches An [integer()] that identifies the minimum number of acceptable
+#' @param minMatches An [integer] that identifies the minimum number of acceptable
 #' matches that can be found by `pattern` in `x`.
 #' 
-#' @param maxMatches An [integer()] that identifies the maximum number of acceptable
+#' @param maxMatches An [integer] that identifies the maximum number of acceptable
 #' matches that can be found by `pattern` in `x`.
 #' 
-#' @param returnIndex A [logical()] value that toggles whether a [numeric()] or 
-#' [character()] vector is returned by the function. If TRUE (the default), matching elements' 
+#' @param returnIndex A [logical] value that toggles whether a [numeric] or 
+#' [character] vector is returned by the function. If TRUE (the default), matching elements' 
 #' indices will be returned by the function. Otherwise, the function will return 
 #' the actual elements within `x` that match `pattern`. (This is the complement of 
 #' the `value` argument in [grep()])
 #' 
-#' @param filePath An optional `character()` string (with default value `NULL`). This should 
+#' @param filePath An optional `character` string (with default value `NULL`). This should 
 #' be the path to the file that is the source of `x`. See 'Details' for more information. 
 #' 
-#' @param ignore.case A [logical()] value that affects case sensitivity in the pattern 
+#' @param ignore.case A [logical] value that affects case sensitivity in the pattern 
 #' matching algorithm. Exactly like [grep()], this argument is FALSE by default, 
 #' causing `pattern` to be case sensitive in its search (e.g., "A" and "a" are 
 #' treated differently). If this argument is TRUE, case is ignored during matching 
 #' (e.g., "A" and "a" are equivalent). 
 #' 
-#' @param fixed A [logical()] value that determines whether `pattern` is treated like 
+#' @param fixed A [logical] value that determines whether `pattern` is treated like 
 #' a regular expression. Just like [grep()], this parameter is FALSE by default, 
 #' and `pattern` is applied in matching as a regular expression. Specify `TRUE` if 
 #' literal matches are desired (when TRUE, `fixed` can override other arguments). 
 #' 
-#' @returns Either a [character()] or [integer()] [vector()]. Empty vectors are possible.
+#' @returns Either a [character] or [integer] [vector]. Empty vectors are possible.
 #' 
 #' @export
 #' 
