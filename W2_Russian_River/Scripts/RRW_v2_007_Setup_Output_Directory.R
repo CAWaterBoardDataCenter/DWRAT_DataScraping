9954a3bf183ab01d004fe71ceb264f9c056befb7
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2026-09-17 16:32:15 -0700

    Made identifying the file archive folder a generic function for use in both RR and LSPC workflow

--- W2_Russian_River/Scripts/RRW_v2_007_Setup_Output_Directory.R
+++ W2_Russian_River/Scripts/RRW_v2_007_Setup_Output_Directory.R
@@ -22,7 +22,7 @@
 # It will be a text file containing a single line that specifies 
 # the path to the newly generated directory
 
-# Its filename will be "Hydrology_Output_Folder_[startDate]_[endDate].txt"
+# Its filename will be "Archive_Location_[startDate]_[endDate].txt"
 
 
 #### Setup ####
@@ -99,7 +99,7 @@ mainProcedure <- function () {
   # Save 'outputDirectory' to a text file in the "Output" folder
   # This will make it easier to reference in later scripts
   outputDirectory |>
-    writeOutput(paste0("W2_Russian_River/Output/Hydrology_Output_Location_", startDate,
+    writeOutput(paste0("W2_Russian_River/Output/Archive_Location_", startDate,
                        "_", endDate, ".txt"))
   
   
