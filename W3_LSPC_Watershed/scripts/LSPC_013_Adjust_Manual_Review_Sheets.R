5983d04143ca8f0187798ab22005226b02eb4316
Aakash Prashar <Aakash.Prashar@Waterboards.ca.gov>
2026-09-23 12:23:37 -0700

    Initial draft of QC file adjustment script

--- W3_LSPC_Watershed/scripts/LSPC_013_Adjust_Manual_Review_Sheets.R
+++ W3_LSPC_Watershed/scripts/LSPC_013_Adjust_Manual_Review_Sheets.R
@@ -64,8 +64,8 @@ mainProcedure <- function () {
   reviewPath <- get_from_lspc_master_control("PRIOR_MANUAL_REVIEW_SHEETS_LOCATION")
   
   
-  # If this field is not empty, check whether the folder path is 
-  # a cloud path or local path
+  # If this field is not empty, 
+  # check whether the folder path is a cloud path or local path
   if (!is.na(reviewPath)) {
     
     reviewPath <- reviewPath |>
@@ -122,6 +122,15 @@ mainProcedure <- function () {
                   ". Please investigate."))
   
   
+  # If the temporary file name "~$[NAME].xlsx" appears in 'qcFiles',
+  # output an error message
+  error_if(any(paste0("~$", reqFiles) %in% unlist(qcFiles)),
+           
+           paste0("Spreadsheet Opened by User\n\n",
+                  "One or more of the QC spreadsheets is currently open. Please ",
+                  "close those files before proceeding."))
+  
+  
   cat("\tDone!\n\n")
   
   
@@ -145,10 +154,17 @@ mainProcedure <- function () {
   # Perform some updates on them to support data archiving and faster reviews
   for (i in 1:nrow(controlDF)) {
     
+    paste0("\t\t[", i, "/", nrow(controlDF), "]\t", controlDF$project_name[i], "\n\n") |>
+      cat()
+    
+    
     # Read in the QC workbooks and duplicate their worksheets 
     # (The duplicate will be preserved and remain unchanged)
     # Also, try to incorporate previous manual review decisions
     for (j in 1:length(qcFiles[[i]])) {
+      
+      paste0("\t\t\t[", i, "/", length(qcFiles[[i]]), "]\t", qcFiles[[i]][j], "\n\n") |>
+        cat()
       
       # Get the path to the spreadsheet
       sheetPath <- paste0(qcDir[i], "/", qcFiles[[i]][j])
@@ -195,17 +211,9 @@ mainProcedure <- function () {
       # Before proceeding forward, perform some data validation
       
       
-      # To Do: Validate worksheet
-      if (qcFiles[[i]][j] %in% "Ground_stations_Flagged_1_2_3.xlsx") {
-        
-        stopifnot("Date" %in% names(wb_to_df(wb, sheet = wbNames[1])))
-        
-      } else if (qcFiles[[i]][j] %in% "Flag4_Monthly_Only_CompleteData.xlsx") {
-        
-        stopifnot("Year" %in% names(wb_to_df(wb, sheet = wbNames[1])))
-        stopifnot("Month" %in% names(wb_to_df(wb, sheet = wbNames[1])))
-        
-      }
+      # To Do: Validate QC worksheets
+      wb_to_df(wb, sheet = wbNames[1]) |>
+        check_for_date_cols(qcFiles[[i]][j])
       
       
       # Duplicate the first worksheet in 'wb' to create the archive sheet
@@ -213,7 +221,9 @@ mainProcedure <- function () {
       
       
       # Then, protect that archive worksheet to deter edits
-      wb <- wb_protect_worksheet(wb, worksheetArchiveName, password = "archive")
+      wb <- wb_protect_worksheet(wb, worksheetArchiveName, password = worksheetArchiveName)
+      
+      # (The password to unlock the worksheet is the exact same string as its name)
       
       
       # After that, if 'reviewPath' is not NA, try and see if a corresponding
@@ -252,10 +262,10 @@ mainProcedure <- function () {
   
   
   # After the script completion message, inform the user of the next steps
-  paste0("Manual review spreadsheets have been generated for each watershed. ",
+  paste0("\nManual review spreadsheets have been generated for each watershed. ",
          "They are located within each project watershed's respective QC folder ",
          "(e.g., \"", qcDir[[1]], "\").\n\n",
-         "Please review the two spreadsheets that contain QC Flags 1, 2, 3, and 4.",
+         "Please review the two spreadsheets that contain QC Flags 1, 2, 3, and 4. ",
          "Delete the entries of values that should be removed. Later scripts ",
          "will fill in all blank entries with data from PRISM.") |>
     errWrap() |>
@@ -265,6 +275,60 @@ mainProcedure <- function () {
   
   # Return nothing
   return(invisible(NULL))
+  
+}
+
+
+
+check_for_date_cols <- function (df, filePath) {
+  
+  # Given a QC worksheet (read in as a data frame) and its filepath,
+  # confirm that it has the proper date-related columns
+  
+  
+  # Check if the column(s) are missing
+  df |>
+    checkMissingCol(colNames = qc_expected_date_colnames(filePath))
+  
+  # (The list of expected date columns for a given QC worksheet is specified
+  #  in the function `qc_expected_date_colnames`)
+  
+  
+  # Return nothing
+  return(invisible(NULL))
+  
+}
+
+
+
+qc_expected_date_colnames <- function (filePath) {
+  
+  # For a specific LSPC QC spreadsheet, check if it has the correct date columns
+  
+  # Depending on the specific spreadsheet, the date may be stored in a different format
+  # or split across column names
+  
+  # This function will return a vector containing the expected column names
+  # that are related to the date
+  
+  
+  if (filePath %in% "Ground_stations_Flagged_1_2_3.xlsx") {
+    
+    return("Date")
+    
+  } else if (filePath %in% "Flag4_Monthly_Only_CompleteData.xlsx") {
+    
+    return(c("Year", "Month"))
+    
+  } else {
+    
+    paste0("Unrecognized File Name\n\n",
+           "The file \"", filePath, "\" does not have any information specified ",
+           "in this function. Its expected date-related columns are unknown. ",
+           "Please revise the script.") |>
+      stop_script()
+    
+  }
   
 }
 
@@ -342,10 +406,344 @@ wb_incorporate_old_review <- function (wb, reviewPath, wsName, qcFile, worksheet
   priorWB <- wb_load(priorPath)
   
   
+  # Confirm that 'priorWB' contains worksheets with the same name as 'wb'
+  # (i.e., it should have 'worksheet' and 'worksheetArchiveName')
+  if (!all(c(worksheet, worksheetArchiveName) %in% wb_get_sheet_names(priorWB))) {
+    
+    # Post a warning and return 'wb' without any edits
+    cat("\n\n")
+    paste0("Warning: In \"", reviewPath, "\", the spreadsheet \"", qcFile, 
+           "\" for \"", wsName, "\" has two worksheets: ", 
+           c(worksheet, worksheetArchiveName) |> vec2QuotedStr(),
+           ". The folder that contains prior manual review spreadsheets has the ",
+           "same file, but it does not have both of these worksheets.\n\n",
+           "These worksheets are required for incorporating past review decisions ",
+           "into the newer spreadsheets. Please investigate.") |>
+      errWrap() |>
+      message()
+    cat("\n\n")
+    
+    
+    return(wb)
+    
+  }
   
   
+  # Once all of these initial checks have been passed, 
+  # more checks will be performed
+  
+  # But before that, the actual spreadsheet tables should be read in
   
   
+  # Load in the new manual review spreadsheet's data
+  newReview <- wb_to_df(wb, worksheet)
+  
+  
+  # Read in the edited and unedited versions of the prior review sheet too
+  priorEdited <- wb_to_df(priorWB, worksheet)
+  
+  priorArchive <- wb_to_df(priorWB, worksheetArchiveName)
+  
+  
+  # To Do: Validate QC worksheets
+  newReview |>
+    check_for_date_cols(qcFile)
+  
+  priorEdited |>
+    check_for_date_cols(qcFile)
+  
+  priorArchive |>
+    check_for_date_cols(qcFile)
+  
+  
+  # Confirm that 'priorEdited' and 'priorArchive' have the same dimensions and columns
+  error_if(nrow(priorEdited) != nrow(priorArchive) || 
+             ncol(priorEdited) != ncol(priorEdited) ||
+             !all(names(priorEdited) == names(priorArchive)),
+           
+           paste0("Worksheet Tables Do Not Match\n\n",
+                  "In \"", reviewPath, "\", the old manual review spreadsheet ",
+                  "for \"", wsName, "\" (\"", qcFile, "\") should have two worksheets: ",
+                  c(worksheet, worksheetArchiveName) |> vec2QuotedStr(), ".\n\n",
+                  "These two worksheets should be identical in size and format. ",
+                  "The only major difference should be that that a manual ",
+                  "review was performed and some values were deleted in \"", 
+                  worksheet, "\". However, the two worksheets do not match. ",
+                  "Please investigate."))
+  
+  
+  # For consistency, regardless of the spreadsheet type, temporarily add a "DATE" column
+  # (Sometimes, there's already a "Date" column)
+  # (Alternatively, there's a "Year" and "Month" column, with some merged cell issues)
+  # This will ensure that all worksheets can consistently reference dates through the same way
+  newReview <- newReview |>
+    add_date_column()
+  
+  priorEdited <- priorEdited |>
+    add_date_column()
+  
+  priorArchive <- priorArchive |>
+    add_date_column()
+  
+  
+  # After these checks have been completed, the actual procedure can begin
+  
+  # Identify locations where edits occurred to 'priorEdited'
+  # (These will be entries where 'priorArchive' has a value, but 'priorEdited' does not)
+  
+  # If the corresponding value in 'newReview' matches the value in 'priorArchive',
+  # delete that entry so that it matches 'priorEdited' 
+  
+  
+  # First filter 'priorEdited' and 'priorArchive' to dates before 'startDate'
+  # (The user should review any newly downloaded data)
+  priorEdited <- priorEdited |>
+    filter(DATE < startDate)
+  
+  
+  priorArchive <- priorArchive |>
+    filter(DATE < startDate)
+  
+  
+  # Find all locations where 'priorEdited' is NA, while 'priorArchive' is not
+  deletedLocs <- which(is.na(priorEdited) & !is.na(priorArchive), arr.ind = TRUE) |>
+    data.frame()
+  
+  # 'deletedLocs' will initially contain a matrix with columns called "row" and "col"
+  
+  # These indicate the locations in 'priorArchive' and 'priorEdit' 
+  # where values were deleted
+  
+  # 'deletedLocs' is then converted into a data frame 
+  
+  
+  # If 'deletedLocs' is empty, return 'wb' without any changes
+  if (length(deletedLocs) == 0) {
+    return(wb)
+  }
+  
+  
+  # Otherwise, iterate through each edit and apply similar changes to 'newReview'
+  for (i in 1:nrow(deletedLocs)) {
+    
+    # First, locate the corresponding entry in 'newReview'
+    
+    # Check if the column name in 'priorArchive' can be found in 'newReview'
+    
+    # Extract the column name that corresponds to this iteration's edit
+    priorName <- names(priorArchive)[deletedLocs$col[i]]
+    
+    
+    # If it's not present in 'newReview', skip to the next edit
+    if (priorName %notin% names(newReview)) {
+      
+      # Notify the user
+      cat("\n\n")
+      paste0("\t\t\t\tSkipping edit to \"", priorName, "\" (not present in the ",
+             "new manual review spreadsheet)") |>
+        cat()
+      cat("\n\n")
+      
+      next
+      
+    }
+    
+    
+    # After that, find the corresponding entry in 'newReview' that matches 
+    # 'priorName' and the row's date
+    newRow <- which(newReview$DATE == priorArchive$DATE[deletedLocs$row[i]])
+    
+    
+    # If this date does not appear in 'newReview', notify the user and skip it
+    if (length(newRow) == 0) {
+      
+      # Send a message to the user
+      cat("\n\n")
+      paste0("\t\t\t\tSkipping edit to \"", priorName, "\" for ", 
+             priorArchive$DATE[deletedLocs$row[i]], " (date not present in ",
+             "the new review spreadsheet)") |>
+        cat()
+      cat("\n\n")
+      
+      next
+      
+    }
+    
+    
+    # 'newRow' should also not have more than one index
+    # (Each date should only appear once within 'newReview')
+    error_if(length(newRow) > 1,
+             
+             paste0("Spreadsheet Error\n\n",
+                    "The \"", qcFile, "\" manual review spreadsheet for ",
+                    wsName, " has the same date (", priorArchive$DATE[deletedLocs$row[i]],
+                    ") more than once in its file. Please investigate."))
+    
+    
+    # After that, compare the values for this gage and date
+    # If 'priorArchive' and 'newReview' have different values, do not edit 'newReview'
+    if (round(priorArchive[deletedLocs$row[i], deletedLocs$col[i]], digits = 3) !=
+        round(newReview[[priorName]][newRow], digits = 3)) {
+      
+      # Send a message to the user
+      cat("\n\n")
+      paste0("\t\t\t\tSkipping edit to \"", priorName, "\" for ", 
+             priorArchive$DATE[deletedLocs$row[i]], " (the value has changed ",
+             "since the prior review was completed)") |>
+        cat()
+      cat("\n\n")
+      
+      next
+      
+    }
+    
+    
+    # Since the same gage, date, and value are all present in 'newReview',
+    # delete the entry in the new review sheet to match 'priorEdited'
+    newReview[[priorName]][newRow] <- NA_real_
+    
+  }
+  
+  
+  # Once 'newReview' has been updated with the edits in the prior manual review,
+  # save these changes to 'wb'
+  
+  
+  # First, remove the "DATE" column for consistency
+  newReview <- newReview |>
+    select(-DATE)
+  
+  
+  # Then, write it to 'wb'
+  wb <- wb_add_data(wb, sheet = worksheet, newReview, na = NULL)
+  
+  
+  # Include 'priorEdited' and 'priorArchive' in this workbook too
+  
+  # They will need their own unique worksheet names too
+  editedWorksheet <- "Prior_Review"
+  archivedWorksheet <- "Prior_Review_Archive"
+  
+  
+  wb <- wb_add_worksheet(wb, editedWorksheet)
+  wb <- wb_add_worksheet(wb, archivedWorksheet)
+  
+  
+  # Write the data frames to these worksheets
+  wb <- wb_add_data(wb, editedWorksheet, priorEdited, na = NULL)
+  
+  wb <- wb_add_data(wb, archivedWorksheet, priorArchive, na = NULL)
+  
+  
+  # Lock the two worksheets too
+  wb <- wb_protect_worksheet(wb, editedWorksheet, password = worksheetArchiveName)
+  
+  wb <- wb_protect_worksheet(wb, archivedWorksheet, password = worksheetArchiveName)
+  
+  # (The password again is the name of the new review's archive worksheet)
+  
+  
+  # Return 'wb' afterwards
+  return(wb)
+  
+}
+
+
+
+add_date_column <- function (qcDF) {
+  
+  # Some QAQC worksheets have a "Date" column, while others use "Year" and "Month"
+  
+  # For consistency, create a "DATE" column in all data frames
+  
+  # In addition, for worksheets that have "Year" and "Month" columns, 
+  # there may be NA values if merged cells were used in the spreadsheet
+  
+  # This script will fill in those missing values too
+  
+  
+  # Start by checking if "Date" already exists in 'qcDF'
+  # In that case, just define "DATE" to match "Date" and return 'qcDF'
+  if ("Date" %in% names(qcDF)) {
+    
+    return(qcDF |>
+             mutate(DATE = Date))
+    
+  }
+  
+  
+  # Output an error message if 'qcDF' lacks "Year" and "Month" columns
+  error_if(any(c("Year", "Month") %notin% names(qcDF)),
+           
+           paste0("Unrecognized Date Names\n\n",
+                  "A QC worksheet is expected to have either \"Date\" or ",
+                  "the pair \"Year\" and \"Month\" in its table. These names ",
+                  "must match exactly. However, one or more of these column ",
+                  "names were not found in the table.\n\n",
+                  "The names that were found instead are: ",
+                  names(qcDF) |> vec2QuotedStr(), "\n\n",
+                  "Please investigate.\n\n"))
+  
+  
+  # For "Year" and "Month" pairs, there may be "NA" values in "Year"
+  # This can happen if there are merged cells in use for "Year"
+  # When that worksheet is converted into a data frame, the column gains blanks
+  if (anyNA(qcDF$Year)) {
+    
+    # Add a temporary "YEAR2" column to 'qcDF'
+    # That's where the NA values will be filled in
+    # (The original "Year" column will not be modified)
+    qcDF <- qcDF |>
+      mutate(YEAR2 = Year)
+    
+    
+    # The next step will be to fill in the NA gaps
+    # Use a `for` loop for that
+    
+    # Go down the rows of "Year" values
+    # When "Year" is not NA, update a variable called 'replacementYear'
+    # If "Year" is NA, substitute it with 'replacementYear'
+    
+    # That way, cases like "2023 NA NA 2024 NA" become "2023 2023 2023 2024 2024"
+    
+    replacementYear <- NA_real_
+    
+    
+    # Update values using a loop
+    for (i in 1:nrow(qcDF)) {
+      
+      # If this row's iteration of "YEAR2" is not NA, update 'replacementYear'
+      if (!is.na(qcDF$YEAR2[i])) {
+        
+        replacementYear <- qcDF$YEAR2[i]
+        
+      # Otherwise, if this row's value for "YEAR2" is NA, substitute in 'replacementYear'
+      } else {
+        
+        qcDF$YEAR2[i] <- replacementYear
+        
+      }
+      
+      
+    }
+    
+    
+    # Define a "DATE" column next
+    # Use "YEAR2" and "Month" for that
+    qcDF <- qcDF |>
+      mutate(DATE = paste0(YEAR2, "-", Month) |>
+               as_date(format = "%Y-%m"))
+      
+    
+    # Remove the "YEAR2" column afterwards
+    qcDF <- qcDF |>
+      select(-YEAR2)
+    
+  }
+  
+  
+  # Return 'qcDF'
+  return(qcDF)
   
 }
 
